4dafca4e750afd9cb50ac7ee7ca49a5212546890
Michael Ream <mrmream@gmail.com>
2023-01-31 15:11:16 -0700

    Made it look more fun, added mouse functionality to canvas

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 const canvas = document.querySelector('#canvas');
+
+
+canvas.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+    let color = "black";
+    let size = 5;
+  
+    let pixel = document.createElement("div");
+    pixel.style.backgroundColor = color;
+    pixel.style.width = `${size}px`;
+    pixel.style.height = `${size}px`;
+    pixel.style.position = "absolute";
+    pixel.style.left = `${x - size / 2}px`;
+    pixel.style.top = `${y - size / 2}px`;
+  
+    canvas.appendChild(pixel);
+  });
+  
+const resetButton = document.querySelector("#reset-button");
+
+resetButton.addEventListener("click", () => {
+  canvas.innerHTML = "";
+});
