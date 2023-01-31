cfed81f1b3f4956e80b17b95041a00587052eff9
Michael Ream <mrmream@gmail.com>
2023-01-31 15:49:51 -0700

    Looks more fun now, changed color to aqua, can change pixel size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const canvas = document.querySelector('#canvas');
 
-
 canvas.addEventListener("mousemove", (e) => {
     let x = e.pageX;
     let y = e.pageY;
-    let color = "black";
-    let size = 5;
-  
+    let color = "aqua";
+    let size = document.querySelector('input[name="width"]:checked').value;
+
+    if (x >= canvas.offsetLeft + canvas.offsetWidth || x <= canvas.offsetLeft || 
+        y >= canvas.offsetTop + canvas.offsetHeight || y <= canvas.offsetTop) {
+        return;
+    }
+    
     let pixel = document.createElement("div");
     pixel.style.backgroundColor = color;
     pixel.style.width = `${size}px`;
@@ -14,12 +18,17 @@ canvas.addEventListener("mousemove", (e) => {
     pixel.style.position = "absolute";
     pixel.style.left = `${x - size / 2}px`;
     pixel.style.top = `${y - size / 2}px`;
-  
+    
     canvas.appendChild(pixel);
   });
-  
+
 const resetButton = document.querySelector("#reset-button");
 
 resetButton.addEventListener("click", () => {
   canvas.innerHTML = "";
 });
+
+
+
+
+
